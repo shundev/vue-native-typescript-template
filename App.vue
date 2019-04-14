3500de4b044af409d019371697f761ff8d4b8d6f
Shunsuke HONDO <shunsuke.hondo@gmail.com>
2019-04-14 11:26:20 +0900

    init

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  view.container
+    text.title
+      | Counter example
+    counter
+</template>
+
+<script lang="ts">
+import Counter from './components/Counter.vue';
+
+export default {
+  components: { Counter },
+};
+</script>
+
+<style lang="stylus">
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+
+.title {
+  font-size: 18px;
+  margin-bottom: 20px;
+}
+</style>
---
+++ README.md
@@ -0,0 +1,24 @@
+# Vue native with Typescript template
+
+This project is based on:
+
+* Vue
+* Vue native
+* Typescript
+* Expo
+
+## Install 
+
+```
+git clone git@github.com:shunsukehondo/vue-native-typescript-template.git your_project
+cd your_project
+sudo sysctl -w kern.maxfiles=5242880
+sudo sysctl -w kern.maxfilesperproc=524288
+yarn install
+```
+
+## Run
+
+```
+yarn run # ios / android
+```
